bea12d9dfb697f9a5e969c8d356e9c681b75873b
Rajesh Prajapati <prajapatirajesh2003@gmail.com>
2024-09-09 13:09:42 +0530

    react query for server side state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './services/store.js'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+
+// Create a QueryClient instance for React Query
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap with both Redux and React Query Providers */}
     <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
       <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 )
